23019c3fb38b4b834142e27634fa2b11a166df3d
RichardPanigor <richardpanigors02@gmail.com>
2021-03-23 20:17:50 +0700

    Commit

---
+++ HaloIndonesia.java
@@ -0,0 +1,13 @@
+// Nama  : Richard Panigor Sitompul
+// Kelas : 2A-Sistem Informasi
+// NIM   : 11200930000025
+package halo;
+
+public class HaloIndonesia
+{
+    public static void main(String[] args){
+        System.out.println("Halo Indonesia");
+        System.out.println("Selamat Pagi Indonesia");
+        System.out.println("Jaya Indonesia");
+    }
+}
